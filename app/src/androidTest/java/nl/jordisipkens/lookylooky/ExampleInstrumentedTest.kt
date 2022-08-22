e92e4832376edb85c04ed789bc9836e611731bd9
Jordi Sipkens <jordi.sipkens@sogeti.com>
2022-08-22 11:45:55 +0200

    Ininitial push

---
+++ app/src/androidTest/java/nl/jordisipkens/lookylooky/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package nl.jordisipkens.lookylooky
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("nl.jordisipkens.lookylooky", appContext.packageName)
+    }
+}
