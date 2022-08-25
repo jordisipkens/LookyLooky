7e9982a7cecbc3dd051d7f8885eebd41f2424381
Jordi Sipkens <jordisipkens@gmail.com>
2022-08-25 09:11:50 +0200

    Redesigned to make landscape friendly

--- app/src/main/java/nl/jordisipkens/lookylooky/features/repos/detail/ReposDetailScreen.kt
+++ app/src/main/java/nl/jordisipkens/lookylooky/features/repos/detail/ReposDetailScreen.kt
@@ -41,24 +41,57 @@ fun ReposDetailScreen(
         is ReposDetailUiState.Empty -> Text("No repo found")
         is ReposDetailUiState.Loaded -> {
             viewModel.fetchEvents()
-            Column(
-                modifier = Modifier.padding(5.dp),
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                DetailScreen(state.repo)
-                Divider(modifier = Modifier.padding(top = 10.dp))
-                when (val eventState = viewModel.eventUiState.collectAsState().value) {
-                    is ReposEventsUiState.Idle -> Text("No events")
-                    is ReposEventsUiState.Loading -> Text("Fetching events for the repository")
-                    is ReposEventsUiState.Loaded -> EventsList(events = eventState.data)
-                    is ReposEventsUiState.Error -> Text(eventState.error)
-                }
+
+            when (val eventState = viewModel.eventUiState.collectAsState().value) {
+                is ReposEventsUiState.Idle -> SetupViews(repo = state.repo, infoMessage = "No events found")
+                is ReposEventsUiState.Loading -> SetupViews(repo = state.repo, infoMessage = "Fetching events for the repository")
+                is ReposEventsUiState.Loaded -> SetupViews(repo = state.repo, events = eventState.data, infoMessage = "There are no events for this reposiroty")
+                is ReposEventsUiState.Error -> SetupViews(repo = state.repo, infoMessage = eventState.error)
             }
+
+
         }
 
     }
 }
 
+@Composable
+private fun SetupViews(repo: RepoEntity, events: List<EventEntity> = emptyList(), infoMessage: String) {
+    LazyColumn(
+        modifier = Modifier.padding(5.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        item {
+            DetailScreen(repo)
+        }
+        item {
+            Divider(modifier = Modifier.padding(top = 10.dp))
+            Text(
+                "Events ",
+                fontWeight = FontWeight.W600,
+                modifier = Modifier.padding(
+                    start = 5.dp,
+                    bottom = 10.dp,
+                    top = 5.dp
+                ).fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+
+        }
+
+        if(events.isEmpty()) {
+            item {
+                Text(infoMessage)
+            }
+        } else {
+            items(events) { event ->
+                EventItem(event)
+            }
+        }
+
+
+    }
+}
 @Composable
 fun DetailScreen(repo: RepoEntity) {
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
@@ -87,86 +120,51 @@ fun DetailScreen(repo: RepoEntity) {
     )
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun EventsList(events: List<EventEntity>) {
-    Column {
-        if (events.isEmpty()) {
-            Text("No events found for this repository.")
-        } else {
+fun EventItem(event: EventEntity) {
+    Card(
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = md_theme_light_background,
+            contentColor = md_theme_light_onBackground
+        ),
+        border = BorderStroke(1.dp, Color.Gray)
+    ) {
+        Row(
+            modifier = Modifier.padding(10.dp)
+        ) {
 
-            LazyColumn(verticalArrangement = Arrangement.SpaceEvenly) {
+            event.avatarUrl?.let {
+                AsyncImage(
+                    model = event.avatarUrl,
+                    contentDescription = "Avatar",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                )
+            }
 
-                stickyHeader {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        Box(
-                            modifier = Modifier.background(md_theme_light_surfaceVariant)
-                                .fillMaxWidth()
-                        ) {
-                            Text(
-                                "Events ",
-                                fontWeight = FontWeight.W600,
-                                modifier = Modifier.padding(
-                                    start = 5.dp,
-                                    bottom = 10.dp,
-                                    top = 5.dp
-                                ).fillMaxWidth(),
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                    }
-                }
-
-                items(events) { event ->
-                    Card(
-                        modifier = Modifier
-                            .padding(5.dp)
-                            .fillMaxWidth(),
-                        colors = CardDefaults.cardColors(
-                            containerColor = md_theme_light_background,
-                            contentColor = md_theme_light_onBackground
-                        ),
-                        border = BorderStroke(1.dp, Color.Gray)
-                    ) {
-                        Row(
-                            modifier = Modifier.padding(10.dp)
-                        ) {
-
-                            event.avatarUrl?.let {
-                                AsyncImage(
-                                    model = event.avatarUrl,
-                                    contentDescription = "Avatar",
-                                    modifier = Modifier
-                                        .size(40.dp)
-                                        .clip(CircleShape)
-                                )
-                            }
-
-                            Column(
-                                verticalArrangement = Arrangement.SpaceBetween,
-                                modifier = Modifier.padding(horizontal = 10.dp)
-                            ) {
-                                CreateRow(
-                                    label = "Event: ",
-                                    value = event.type,
-                                    verticalPadding = 0.dp
-                                )
-                                CreateRow(
-                                    label = "By user: ",
-                                    value = event.actor,
-                                    verticalPadding = 0.dp
-                                )
-                            }
-                        }
-                    }
-                }
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.padding(horizontal = 10.dp)
+            ) {
+                CreateRow(
+                    label = "Event: ",
+                    value = event.type,
+                    verticalPadding = 0.dp
+                )
+                CreateRow(
+                    label = "By user: ",
+                    value = event.actor,
+                    verticalPadding = 0.dp
+                )
             }
         }
     }
 }
+
 
 
 @Composable
